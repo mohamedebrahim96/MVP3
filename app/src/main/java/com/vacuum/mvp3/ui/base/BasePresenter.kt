0baad4e2e5d64a301e973b2285657a4b58f15c7c
mohamed ebrahim <ebrahimm131@gmail.com>
2018-08-23 19:15:08 +0200

    first commit

--- app/src/main/java/com/vacuum/mvp3/ui/base/BasePresenter.kt
+++ app/src/main/java/com/vacuum/mvp3/ui/base/BasePresenter.kt
@@ -3,14 +3,10 @@ package com.vacuum.mvp3.ui.base
 import com.vacuum.mvp3.data.DataManager
 
 
-
 class BasePresenter<V : MvpView>(dataManager: DataManager) : MvpPresenter<V> {
 
     var mvpView: V? = null
         private set
-
-
-
 
 
     override fun onAttach(mvpView: V) {
