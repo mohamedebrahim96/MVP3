35c2a63a24e2dbfa7168ff6484e30548b0fb9d9d
mohamed ebrahim <ebrahimm131@gmail.com>
2018-08-23 19:14:54 +0200

    first commit

--- app/src/main/java/com/vacuum/mvp3/ui/base/BasePresenter.kt
+++ app/src/main/java/com/vacuum/mvp3/ui/base/BasePresenter.kt
@@ -3,6 +3,7 @@ package com.vacuum.mvp3.ui.base
 import com.vacuum.mvp3.data.DataManager
 
 
+
 class BasePresenter<V : MvpView>(dataManager: DataManager) : MvpPresenter<V> {
 
     var mvpView: V? = null
